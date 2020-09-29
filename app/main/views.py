66925071dfd8eea4ab5b21b3116e81d7a3f26ccd
AbugaAroni <abugaaroni@gmail.com>
2020-09-29 16:21:48 +0300

    added base files

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+#from .forms import avwvwe
+#from ..models import cowinqoicnq
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
