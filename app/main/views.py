93100df93af513c50bcfe652ab647b6409c30771
AbugaAroni <abugaaroni@gmail.com>
2020-09-29 23:41:57 +0300

    add working login and sign up form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for, abort
 from . import main
+from flask_login import login_required
+from ..models import User
+from .. import db
 #from .forms import avwvwe
-#from ..models import cowinqoicnq
 
 # Views
 @main.route('/')
@@ -11,3 +13,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
